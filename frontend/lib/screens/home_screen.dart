a442959ecaee4abea229da1ed5cda0b6b32622aa
Halil <nycgamedevs@gmail.com>
2026-08-11 13:20:05 +0300

    Versiyon Guncellemesi: 11.08.2026-13.20

--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -165,7 +165,7 @@ class _HomeScreenState extends State<HomeScreen> {
           const Padding(
             padding: EdgeInsets.all(8.0),
             child: Text(
-              'Versiyon (11.08.2026-13.07)',
+              'Versiyon (%dateStr%)',
               style: TextStyle(color: Colors.white38, fontSize: 12),
             ),
           ),
